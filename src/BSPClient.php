5be5a9078ea366853c62e08c3cd509fd9773a5d3
Thinking Song <123wuming_123@163.com>
2016-07-21 18:40:03 +0800

    init project

---
+++ src/BSPClient.php
@@ -0,0 +1,75 @@
+<?php 
+namespace songlipeng2003\sf\bsp;
+
+use GuzzleHttp\Client;
+
+use FluidXml\FluidXml;
+
+use function FluidXml\fluidxml;
+use function FluidXml\fluidns;
+use function FluidXml\fluidify;
+
+class BSPClient
+{
+    public $accessCode;
+    public $checkWord;
+
+    public $url;
+    private $_client;
+
+    public $debug = false;
+
+    public function OMSClient($accessCode, $checkWord, $url)
+    {
+        $this->accessCode = $accessCode;
+        $this->checkWord = $checkWord;
+        $this->url = $url;
+    }
+
+    public function api($service, $data)
+    {
+        $request = [
+            '@service' => $service,
+            '@lang' => 'zh-CN',
+            'Head' => $this->accessCode,
+            'Body' => $data
+        ];
+
+        $xml = new FluidXml('Request');
+        $xml->add($request);
+
+        if($this->debug){
+            echo 'request:' . $xml;
+        }
+
+        $verifyCode = base64_encode(md5($xml . $this->checkWord, true));
+
+        $client = $this->getClient();
+        $response = $client->post($this->url, ['form_params' => ['xml' => (string)$xml, 'verifyCode' => $verifyCode]]);
+        $code = $response->getStatusCode();
+        if($code==200){
+            $stringBody = (string) $response->getBody();
+
+            if($this->debug){
+                echo 'response:' . $stringBody;
+            }
+
+            $result = simplexml_load_string($stringBody);
+            $result = json_encode($result);
+            $result = json_decode($result, true);
+
+            return $result;
+        }else{
+            $reason = $response->getReasonPhrase();
+        }
+    }
+
+    public function getClient()
+    {
+        if(!$this->_client){
+            $this->_client = new Client([]);
+        }
+
+        return $this->_client;
+    }
+}
---
+++ test/unit/BSPClientTest.php
@@ -0,0 +1,42 @@
+<?php 
+use songlipeng2003\sf\bsp\BSPClient;
+use PHPUnit\Framework\TestCase;
+
+class BSPClientTest extends PHPUnit_Framework_TestCase
+{
+    public function testApi()
+    {
+        $client = new BSPClient();
+        $client->url = 'http://218.17.248.244:11080/bsp-oisp/sfexpressService';
+        $client->accessCode = 'BSPdevelop';
+        $client->checkWord = 'j8DzkIFgmlomPt0aLuwU';
+        $client->debug = true;
+        $request = [
+            'Order' => [
+                '@orderid' => rand(100000, 999999),
+                '@j_company' => '华米科技',
+                '@j_contact' => '宋利鹏',
+                '@j_tel' => '13855106511',
+                '@j_country' => '中国',
+                '@j_province' => '安徽',
+                '@j_city' => '合肥',
+                '@j_county' => '高新区',
+                '@j_address' => '创新产业基地H8栋',
+                '@d_company' => '北京华米科技',
+                '@d_contact' => '测试',
+                '@d_tel' => '电话号码',
+                '@d_province' => '北京',
+                '@d_city' => '北京',
+                '@d_county' => '海淀区',
+                '@d_address' => '上地软件园',
+                'Cargo' => [
+                    '@name' => '小米手环',
+                    '@count' => 1,
+                ]
+            ]
+        ];
+        $result = $client->api('OrderService', $request);
+        $this->assertEquals($result['Head'], 'OK');
+        $this->assertContains($result['Body'], 'OrderResponse');
+    }
+}
